9d4f236a1faec70a1fcd0ae29eb62442fff9cc3c
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-08-14 15:59:52 -0300

    Steps 3 passed

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -7,19 +7,16 @@ public class AprenderCucumber {
 
 	@Given("^que criei o arquivo corretamente$")
 	public void que_criei_o_arquivo_corretamente() throws Throwable {
-		System.out.println("Passou aqui");
 	}
 
 	@When("^execut?-lo$")
 	public void execut_lo() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		
 	}
 
 	@Then("^a especifica??o deve finalizar com sucesso$")
 	public void a_especifica_o_deve_finalizar_com_sucesso() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		
 	}
 
 }
